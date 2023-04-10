efcddab29227712a0a3c65bc611425d2412bd225
EZLight777 <antiblasphemystudios@protonmail.com>
2023-04-10 16:12:37 -0400

    update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import author from "./author";
+import category from "./category";
 import post from "./post";
 
 
-export const schemaTypes = [post, author]
+export const schemaTypes = [post, author, category]
