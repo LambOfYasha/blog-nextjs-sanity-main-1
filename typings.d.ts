1a557e0ebfc75a47916cc417fbcf080e4fb123be
EZLight777 <antiblasphemystudios@protonmail.com>
2023-04-10 15:59:51 -0400

    update

--- typings.d.ts
+++ typings.d.ts
@@ -1,6 +1,14 @@
-export interface Post {
-    _id: string;
+
+type Base = {
+
     _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
+}
+
+interface Post extends Base {
     title: string;
     author: {
         name: string;
@@ -12,4 +20,46 @@ export interface Post {
     };
     slug: {current: string;};
     body: [object];
+}
+
+interface Author extends Base {
+    bio: Block[];
+    image: Image;
+    name: string;
+    slug: Slug;
+}
+
+interface Image {
+    _type: "Image";
+    asset: Reference;
+}
+
+interface Reference {
+    _ref: string;
+    _type: "reference";
+}
+
+interface Slug {
+    _type: "slug";
+    current: string;
+}
+
+interface Block {
+    _key: string;
+    _type: "block";
+    children: Span[];
+    markDefs: any[];
+    style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+}
+
+interface Span {
+    _key: string;
+    _type: "span";
+    marks: string[];
+    text: string;
+}
+
+interface Category extends Base {
+    description: string;
+    title: string;
 }
