57868e3012d055674386ec1cff1c87f2bae663f2
EZLight777 <antiblasphemystudios@protonmail.com>
2023-04-25 00:54:06 -0400

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,12 @@ export default function Home({posts}: Props) {
   return (
     <div>
    <Theme title="News"><div className='w3-container  w3-row-padding w3-section ' style={{margin:"auto", width: '50%'}}>
- {posts.map(post => (
-    <Link key={post._id} href={`/post/${post.slug.current}`}>
+ 
   <article className='w3-card w3-margin w3-col m3 w3-border w3-border-black w3-amber w3-button w3-round-xlarge'>
       <div className="w3-border w3-white w3-panel w3-border-black"><img  alt="" /></div>
       <div className='w3-black'>Title</div>
       <div className='w3-grey'>Date</div>
-  </article></Link>
-  ))}
+  </article>
   </div></Theme>
    </div>
   )
