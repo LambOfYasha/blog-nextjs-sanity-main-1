9851e8d736e416c189f815293eb730b6e7499df9
EZLight <antiblasphemystudios@protonmail.com>
2023-10-20 16:01:21 -0400

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,36 @@
-import { authConfig } from "../../../../lib/auth";
-import NextAuth from 'next-auth/next'
+import { NextResponse } from "next/server";
+import { db } from "../../../../lib/db";
+import { hash } from "bcrypt";
 
-const handler = NextAuth(authConfig)
+export async function POST(req: Request) {
+    try {
+        const body = await req.json()
+        const {email, username, password} = body
+        //check for email
+        const existingUserByEmail = await db.user.findUnique({
+            where: {email: email}
+        })
+        if(existingUserByEmail){
+            return NextResponse.json({user: null, message: "User already exists with email."}, {status: 409})
+        }
+        const existingUserByUsername = await db.user.findUnique({
+            where: {username: username}
+        })
+        if(existingUserByUsername){
+            return NextResponse.json({user: null, message: "User with this username already exists"}, {status: 409})
+        }
 
-export {handler as GET, handler as POST}
+        const hashedPassword = await hash(password, )
+        const newUser = await db.user.create({
+            data: {
+                username,
+                email,
+                password
+            }
+        })
+        return NextResponse.json(body)
+    } catch(error){
+        
+    }
+}
+
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,46 +1,58 @@
+import {NextAuthOptions, User, getServerSession} from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google'
+import bcrypt from 'bcrypt'
+import { db } from './db';
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import Credentials from "next-auth/providers/credentials"
-import { User } from "sanity"
-import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 
-const prisma = new PrismaClient()
+
+
 export const authConfig: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(db),
     providers: [
-        Credentials({
-            name: "ABStudios",
+        CredentialsProvider({
+            name: "Sign In",
             credentials: {
                 email: {
                     label: "Email",
                     type: "email",
                     placeholder: "example@example.com"
                 },
-                password: {label: "Password", type: "password"},
+                password: {
+                    label: "Password",
+                    type: "password"
+                },
             },
-            async authorize(credentials) {
-                if(!credentials || !credentials.email || !credentials.password)
-                return null
+                 async authorize(credentials){
+                    if(!credentials || !credentials.email || credentials.password)
+                    return null
 
-                const dbUser = await prisma.user.findFirst({
-                    where: {email: credentials.email},
-                })
+                    const dbUser = await db.user.findFirst({
+                        where: {email: credentials.email}
+                    })
 
-
-                if(dbUser && dbUser.password === credentials.password){
-                    const{ password, createdAt, id, ...dbUserWithoutPassword } = dbUser
-                    return dbUserWithoutPassword as User
-                }
-
-                return null
-            },
-        }),
-        GoogleProvider(
-        {
+                    const existingUser = await db.user.findUnique({
+                        where: {email: credentials?.email}
+                    })
+                    if(!existingUser){
+                        return null
+                    }
+                    const passwordMatch = await compare(credentials.password, existingUser.password)
+                 }, 
+            }),
+            GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         
-        }
-    )],
+        })
+    ],
+
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+     debug: process.env.NODE_ENV === 'development',
+        
 }
+
+
