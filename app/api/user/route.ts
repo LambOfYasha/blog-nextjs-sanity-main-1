b499e35fbcafb1616e5638a2bbef139cf88bab5f
EZLight <antiblasphemystudios@protonmail.com>
2023-10-20 20:46:46 -0400

    update

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,24 @@
 import { NextResponse } from "next/server";
 import { db } from "../../../lib/db";
 import { hash } from "bcrypt";
+import * as z from 'zod'
+
+//Define schema for input validation
+
+const userSchema = z
+.object({
+    username: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+    .string()
+    .min(1, 'Password is required')
+    .min(8, 'Password must have more than 8 characters'),
+})
 
 export async function POST(req: Request) {
     try {
         const body = await req.json()
-        const {email, username, password} = body
+        const {email, username, password} = userSchema.parse(body)
         //check for email
         const existingUserByEmail = await db.user.findUnique({
             where: {email: email}
@@ -31,7 +44,7 @@ export async function POST(req: Request) {
         return NextResponse.json({user: newUser, message: "User created successfully"},
         {status: 201})
     } catch(error){
-        
+        return NextResponse.json({message: "Something went wrong"}, {status: 500})
     }
 }
 
