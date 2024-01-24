e14decffbdf87796b1fda965fdc672db45d2f84b
EZLight <antiblasphemystudios@protonmail.com>
2024-01-24 16:33:40 -0500

    update

--- app/projects/greedy-salvation/characters/page.tsx
+++ app/projects/greedy-salvation/characters/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image"
 import urlFor from "../../../../lib/urlFor"
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import GreedySalvationMenu from "../../../../components/GreedySalvationMenu"
+import { Key } from "react"
 
 
 
@@ -30,12 +31,12 @@ const character: Character = await client.fetch(query)
        <div className="w3-bar">
          <Tabs variant='enclosed'>
   <TabList>
-   {character.map((chrInfo, index) => (
+   {character.map((chrInfo: { avi: any; title: string }, index: Key | null | undefined) => (
           <Tab key={index} className="w3-amber"> <Image className="w3-hide-small w3-circle w3-image w3-border w3-border-black" width={100} height={200} src={urlFor(chrInfo.avi).url()} alt={chrInfo.title} /></Tab>
    ))}
   </TabList>
   <TabPanels>
-  {character.map((chrInfo, index) => (
+  {character.map((chrInfo: { coverImage: any; title: string }, index: Key | null | undefined) => (
     <TabPanel key={index}>
            
             <Image className="w3-hide-small w3-image" width={1200} height={1000} src={urlFor(chrInfo.coverImage).url()} alt={chrInfo.title} />
