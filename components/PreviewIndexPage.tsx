818776d8cdc22984b1dbae22a286751733157c35
EZLight <antiblasphemystudios@protonmail.com>
2024-02-08 18:53:18 -0500

    update

--- components/PreviewIndexPage.tsx
+++ components/PreviewIndexPage.tsx
@@ -1,11 +1,11 @@
-import IndexPage from 'components/IndexPage'
-import { usePreview } from 'lib/sanity.preview'
+import IndexPage from './IndexPage'
+import { usePreview } from '../lib/sanity.preview'
 import {
   indexQuery,
   type Post,
   type Settings,
   settingsQuery,
-} from 'lib/sanity.queries'
+} from '../lib/sanity.queries'
 
 export default function PreviewIndexPage({ token }: { token: null | string }) {
   const posts: Post[] = usePreview(token, indexQuery) || []
