dada1e93e4253d7d8f8ec57ae1cfa8b38dc1ed2c
EZLight <antiblasphemystudios@protonmail.com>
2024-02-08 18:48:32 -0500

    update

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -1,7 +1,16 @@
-export default function PostTitle({ children }) {
+import React, {FC, ReactNode} from 'react'
+
+interface Props {
+  children?: ReactNode
+}
+
+export const PostTitle: FC<Props> = ({ children, ...props }) => {
   return (
-    <h1 className="mb-12 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl">
+    <h1 className="mb-12 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl" {...props}>
       {children}
     </h1>
   )
 }
+
+
+export default PostTitle
