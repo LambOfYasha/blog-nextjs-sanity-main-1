5dbf6fd13e739cfa0c6fa074da1c4ff88c3c2207
EZLight <antiblasphemystudios@protonmail.com>
2024-02-13 23:15:59 -0500

    update

--- app/mobile/page.tsx
+++ app/mobile/page.tsx
@@ -1,8 +1,8 @@
 import { groq } from "next-sanity"
 import {client} from '../../lib/sanity.client'
 import { HeaderTitleBar, HeaderTitleBarMobile } from "../../styles/styles"
-import PortablePost from "../../components/Modal"
 import BlogBox from "../../components/BlogBox"
+import Link from "next/link"
 
 
  const query = groq`
@@ -26,6 +26,8 @@ export default async function Page(){
 <article className=" w3-center">
     <h4 className={HeaderTitleBarMobile}>Home</h4>
      <BlogBox posts={posts}  />
+     <Link className="w3-center" href={'https://www.antiblasphemy.studio/page2'} ><p>Next Page</p></Link>
+
 </article> 
 )
 }
---
+++ app/mobile/page2/page.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { groq } from "next-sanity"
+import { client } from "../../../lib/sanity.client"
+import BlogBox from "../../../components/BlogBox"
+import Link from "next/link"
+import { HeaderTitleBarMobile } from "../../../styles/styles"
+
+ const query = groq`
+  *[_type=='post'][10...20]{
+    ...,
+    coverImage,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`
+
+
+
+export default async function Page() {
+
+
+
+const posts = await client.fetch(query)
+return<section>
+    <h4 className={HeaderTitleBarMobile}>Home</h4>
+  <BlogBox posts={posts}  />
+  <Link className="w3-center" href={'https://www.antiblasphemy.studio/'} ><p>Prev Page</p></Link>
+</section>}
+
+
