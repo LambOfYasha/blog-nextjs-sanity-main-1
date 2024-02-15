dad668b2f76c858a95a16313c844ab06beaf5616
EZLight <antiblasphemystudios@protonmail.com>
2024-02-15 11:11:32 -0500

    update

--- styles/images.js
+++ styles/images.js
@@ -1,5 +1,5 @@
 export var AbsLogo="/assets/ABSLogo.png"
 export var ABSTabletBanner="/assets/ABS_banner-tablet.png"
-export var ABSDesktopBanner="/assets/ABS_banner-desktop.png"
+export var ABSDesktopBanner="/assets/ABSBanner.png"
 export var LoadingIcon="/assets/loading.png"
 export var GreedySalvationBanner="/assets/GSBanner.png"
