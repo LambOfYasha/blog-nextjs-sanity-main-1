4b57f156676673405f4f00c6be8a4819dd2c0fb0
EZLight <antiblasphemystudios@protonmail.com>
2024-07-04 09:17:13 -0400

    update

--- app/mobile/projects/page.tsx
+++ app/mobile/projects/page.tsx
@@ -32,7 +32,7 @@ export default async function Page(){
     <PortableText value={page.content} ></PortableText>
     <Link href={"https://www.antiblasphemy.studio/mobile/projects/greedy-salvation"} ><Image src={GreedySalvationBanner} className='w3-image w3-hide-large w3-hide-medium' alt="GreedySalvation" width="300" height="50" />
 </Link>
-<Link href={"https://www.antiblasphemy.studio/projects/zin"} ><Image src={ZINBanner} className='w3-image w3-hide-large w3-hide-medium' alt="ZIN" width="300" height="50" /></Link>
+<Link href={"https://www.antiblasphemy.studio/mobile/projects/zin"} ><Image src={ZINBanner} className='w3-image w3-hide-large w3-hide-medium' alt="ZIN" width="300" height="50" /></Link>
 
 </article> 
 )
