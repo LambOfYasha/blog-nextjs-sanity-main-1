7d7b38c6297fde0fb5c7d84fc30784f28a293383
EZLight <antiblasphemystudios@protonmail.com>
2024-07-04 09:15:19 -0400

    update

--- app/mobile/projects/page.tsx
+++ app/mobile/projects/page.tsx
@@ -6,7 +6,7 @@ import { JSXElementConstructor, Key, ReactElement, ReactFragment, ReactPortal } 
 import { HeaderTitleBar, HeaderTitleBarMobile } from "../../../styles/styles"
 import { PortableText } from "@portabletext/react"
 import PageModal from "../../../components/PageModal"
-import { GreedySalvationBanner } from "../../../styles/images"
+import { GreedySalvationBanner, ZINBanner } from "../../../styles/images"
 import Link from "next/link"
 import Image from "next/image"
 
@@ -32,6 +32,8 @@ export default async function Page(){
     <PortableText value={page.content} ></PortableText>
     <Link href={"https://www.antiblasphemy.studio/mobile/projects/greedy-salvation"} ><Image src={GreedySalvationBanner} className='w3-image w3-hide-large w3-hide-medium' alt="GreedySalvation" width="300" height="50" />
 </Link>
+<Link href={"https://www.antiblasphemy.studio/projects/zin"} ><Image src={ZINBanner} className='w3-image w3-hide-large w3-hide-medium' alt="ZIN" width="300" height="50" /></Link>
+
 </article> 
 )
 }
