8224ec2b264d5db15cbc99af4897cf3636047a23
EZLight <antiblasphemystudios@protonmail.com>
2024-09-06 14:13:50 -0400

    update

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -6,7 +6,6 @@ import Image from "next/image"
 import { HeaderTitleBar } from "../../../../styles/styles"
 import { useState, useEffect } from "react"
 import ImageUrlBuilder from "@sanity/image-url"
-import YouTubeEmbed from "../../../../components/YouTubeEmbed"
 
 
 export const revalidate = 30
@@ -41,7 +40,26 @@ type Post = {
 }
 
 
-
+const YouTubeEmbed: React.FC<{ value: { url: string } }> = ({ value }) => {
+    const { url } = value;
+    if (!url) return null;
+  
+    // Extract video ID from URL
+    const videoId = url.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/watch\?.+&v=))([\w-]{11})/)?.[1];
+  
+    if (!videoId) return null;
+  
+    return (
+      <div className="youtube-embed">
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    );
 
 function urlFor(source: any) {
     return ImageUrlBuilder(client).image(source)
