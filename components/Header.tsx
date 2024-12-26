fa95bfc875081c77ca7e15ec188659e15582b73d
LambOfYasha <lambofyashasoftwares@protonmail.com>
2024-12-26 13:09:29 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,7 @@
 "use client"
 import React from 'react'
-import * as Navigation from '../styles/styles'
-import Link from 'next/link'
 import NavLinks from '../data/data'
-import NavLink2 from '../data/ministrynav'
-import Image from 'next/image'
 import HomeButtons from './WebElements/Buttons'
-import SignInButton from './SignInButton'
-import { width } from './OpenGraphImage'
-import { ABSDesktopBanner} from '../styles/images'
-
-
-function myClick() {
-    const x = document.getElementById("Click");
-    if (x) {
-        if (!x.className.includes("w3-show")) { 
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
-  }
 
 
 
