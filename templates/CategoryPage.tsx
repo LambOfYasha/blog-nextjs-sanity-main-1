04e60f0f394902d8ccea1903936da21178b5b10e
LambOfYasha <lambofyashasoftwares@protonmail.com>
2024-12-26 12:38:35 -0500

    update

---
+++ templates/CategoryPage.tsx
@@ -0,0 +1,10 @@
+// Create reusable template components
+export default function CategoryPage({ 
+  title, 
+  content,
+  // ... other props 
+}) {
+  return (
+    // ... your reusable layout
+  )
+} 
