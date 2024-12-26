c37d03965a599f5e380c2004413ea99f26d1d923
LambOfYasha <lambofyashasoftwares@protonmail.com>
2024-12-26 13:54:11 -0500

    Update page.tsx

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -2,7 +2,7 @@
 import { groq } from "next-sanity"
 import {client} from '../../lib/sanity.client'
 import { PortableText } from "@portabletext/react"
-import { Center, Square, Circle } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
 import Image from "next/image"
 import urlFor from "../../lib/urlFor"
 import React from "react"
