bc25802e354e2994804a3d57c0cd289041f31269
LambOfYasha <lambofyashasoftwares@protonmail.com>
2024-12-26 12:35:23 -0500

    Update page.tsx

--- app/(user)/categories/ministry/page.tsx
+++ app/(user)/categories/ministry/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
 const posts = await client.fetch(query)
 
 return<section>
- <CategoryMenu />
+ <CategoryMenu /> 
 
        <ArticleBox posts={posts} ></ArticleBox>
   <Link className="w3-center w3-button w3-amber w3-hover-black w3-hover-text-amber w3-border w3-border-black w3-hover-border-amber" href={'/'} ><p>Home</p></Link>
