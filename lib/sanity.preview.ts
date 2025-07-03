e1b7cfffee4fa48deb35e9a2bd5d561db16b0118
LambOfYasha <lambofyashasoftwares@protonmail.com>
2025-07-03 19:05:04 -0400

    update

--- lib/sanity.preview.ts
+++ lib/sanity.preview.ts
@@ -1,5 +1,5 @@
 import { dataset, projectId } from '../lib/sanity.client'
-import { definePreview } from 'next-sanity/preview'
+import definePreview from 'next-sanity/preview'
 
 function onPublicAccessOnly() {
   throw new Error("unable to load preview as you're not logged in.")
