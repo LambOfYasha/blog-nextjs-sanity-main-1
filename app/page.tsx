f755d2ce2fee1d3ea68bcd838a2b82dfd8610c36
LambOfYasha <lambofyashasoftwares@protonmail.com>
2025-07-04 10:48:02 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import { ABMVideo } from "../styles/images"
+
 export default function Home() {
   return (
-    <main className="fixed inset-0 w-screen h-screen overflow-hidden">
-      <iframe
-        src="https://www.youtube.com/embed/zr6IpUM7p7M?autoplay=1&loop=1&playlist=zr6IpUM7p7M&mute=1&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1"
-        title="Embedded Video"
-        className="fixed inset-0 w-screen h-screen"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
-    </main>
+    <div className="w-full h-screen relative">
+      
+      <video src={ABMVideo} autoPlay loop muted className="w-full h-full object-cover" />
+ 
+        <h1 className="w3-text-amber z-10 w3-jumbo w3-center w3-animate-fading" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+         <b> Under Construction</b>
+        </h1>
+ 
+    </div>
   )
 } 
